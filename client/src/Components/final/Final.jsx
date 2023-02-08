869bb652f99c922ce6e563571699f84e313fc9ba
raushan kuamr <sraushan890@gmail.com>
2023-02-09 01:40:44 +0530

    footer and order components done

--- client/src/Components/final/Final.jsx
+++ client/src/Components/final/Final.jsx
@@ -3,7 +3,13 @@ import style from "./final.module.css";
 
 function Final() {
   return (
-    <div>Final</div>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <h2>You have successfull made an order!</h2>
+        <p>Delivery usually between 2-4 days</p>
+      </div>
+      
+    </div>
   )
 }
 
--- client/src/Components/footer/Footer.jsx
+++ client/src/Components/footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import style from "./footer.module.css";
-
+import {AiOutlineInstagram,AiOutlineFacebook,AiOutlineTwitter} from "react-icons/ai"
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <div className={style.column}>
+          <h2>FAQ</h2>
+          <span>What we sell</span>
+          <span>How can you order</span>
+          <span>What currency we accept</span>
+          <span>Privacy policy</span>
+        </div>
+        <div className={style.column}>
+          <h2>About</h2>
+          <p className={style.aboutParagraph}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repudiandae magnam molestias dolorem praesentium itaque rem, iste,
+            distinctio, voluptate doloribus corporis omnis facere dolores
+            reiciendis! Debitis maxime necessitatibus assumenda molestiae ex.
+          </p>
+        </div>
+        <div className={style.column}>
+          <h2>Contact</h2>
+          <div className={style.icons}>
+            <AiOutlineInstagram />
+            <AiOutlineFacebook />
+            <AiOutlineTwitter />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
